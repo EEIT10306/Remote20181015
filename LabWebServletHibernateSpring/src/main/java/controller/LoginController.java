6f3bc34dc1b2dd5d42ae9b7e4a95598470cd7d39
陳仕康 <fwwf100@yahoo.com.tw>
2018-10-15 14:25:48 +0800

    2222

--- LabWebServletHibernateSpring/src/main/java/controller/LoginController.java
+++ LabWebServletHibernateSpring/src/main/java/controller/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController {
 	@RequestMapping("/secure/login.controller")
 	public String method(String username, String password, Model model, Locale locale) {
 //接收資料
-//驗證資料
+//驗證資料HAHAHAH
 		Map<String, String> errors = new HashMap<String, String>();
 		model.addAttribute("errors", errors);
 		
@@ -43,7 +43,7 @@ public class LoginController {
 			return "login.errors";
 		}
 		
-//呼叫model
+//呼叫model 22222
 		CustomerBean bean = customerService.login(username, password);
 		
 //根據model執行結果，導向view
